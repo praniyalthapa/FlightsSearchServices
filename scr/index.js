ac490c6d2fd18421405c788a99c76c782c35ca62
Praniyal Thapa <praniyalthapa100@gmail.com>
2023-10-21 22:40:01 +0545

    done city_repos.js file

--- scr/index.js
+++ scr/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const  {PORT}=require('./config/serverConfig');
-const CityRepository=require('./repository/city_respos');
-const {City}=require('./models/index');
+// const CityRepository=require('./repository/city_respos');
+
 const bodyParser=require("body-parser");
 
 const setupAndStartServer=async()=>{
@@ -12,10 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(PORT,async()=>{
     console.log(`Server started at  ${PORT}`);
-     const repo=new CityRepository();
-    //  repo.createCity({
-    //     name: "Kathmandu"
-    //  });
+     
      
 });
 
--- scr/repository/city_respos.js
+++ scr/repository/city_respos.js
@@ -1,28 +1,55 @@
 //https://sequelize.org/docs/v7/querying/insert/
 const {City}= require('../models/index');
 class CityRepository{
-    async createCity({name}){
+    async createCity({name}){ //{name:"kathmandu"} 
         try{
-            const city=await City.create({name});
-           return city;
+                const city=await City.create({
+                    name:name  //
+                });
+                return city;
         }
         catch(error){
-            throw{error};
-
+       console.log("Something went wrong!");
+       throw{error};
         }
     }
     async deleteCity(cityId){
         try{
-             await City.distroy({
+            await City.distroy({
                 where:{
                     id:cityId
                 }
-             });
+            });
+           return true;
         }
         catch(error){
+            console.log("Something went wrong!");
             throw{error};
-        
         }
+
     }
-}
+    async updateCity(cityId,data){  //to update a particular city you have to fetch what data you want to update we have passed two parameters cityid and data as there is only one object inside city.js file i.e name
+              try {
+                const city=await City.update(data,{ ////this .update wants two parameter one is any data and next one is object which have where clause as shown;
+                    where:{
+                        id:cityId
+                    }
+                });
+              } catch (error) {
+                console.log("Something went wrong!");
+                throw{error};
+              }
+    }
+    async getCity(cityId){
+   try {
+    const city=await City.findByPk(cityId);
+    return city;
+    
+   } catch (error) {
+    console.log("Something went wrong!");
+    throw{error};
+   }
+    }
+   
+} 
 module.exports=CityRepository;
