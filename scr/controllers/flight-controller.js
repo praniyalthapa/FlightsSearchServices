739693995027c50cf5d8e04a855ab6dd0cea0cb9
Praniyal Thapa <praniyalthapa100@gmail.com>
2023-11-04 23:05:43 +0545

    added flight controller middleware

--- scr/controllers/flight-controller.js
+++ scr/controllers/flight-controller.js
@@ -4,9 +4,14 @@ const create=async(req,res)=>{
     try {
         let flightRequestData={
             flightNumber:req.body.flightNumber,
-            
+            airplaneId:req.body.airplaneId,
+            departureAirportId:req.body.departureAirportId,
+            arrivalAirportId:req.body.arrivalAirportId,
+            arrivalTime:req.body. arrivalTime,
+            departureTime:req.body.departureTime,
+            price:req.body.price
         }
-        const flight=await flightService.createFlight(req.body);
+        const flight=await flightService.createFlight(flightRequestData);
         return res.status(201).json({
             data:flight,
             success:true,
